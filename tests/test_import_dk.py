db87b1a1c8434b123a4be82f78659761b74bc5f2
Erlend Dalen <erlend.dalen@gmail.com>
2019-01-07 10:31:38 +0100

    Google image charts
    They are deprecated, but still works. (code was moved from
    datakortet.core).

--- tests/test_import_dk.py
+++ tests/test_import_dk.py
@@ -20,6 +20,7 @@ import dk.js.js
 import dk.dkimport
 import dk.dklogger
 import dk.fstr
+import dk.gchart
 import dk.grid
 import dk.proxy
 import dk.text
@@ -47,6 +48,7 @@ def test_import_dk():
     assert dk.dkimport
     assert dk.dklogger
     assert dk.fstr
+    assert dk.gchart
     assert dk.grid
     assert dk.proxy
     assert dk.text
