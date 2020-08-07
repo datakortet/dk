79dd8e5ab37ff623c48c23e8d63f72274f233c90
Bjorn Pettersen <bp@norsktest.no>
2020-08-07 13:13:24 +0200

    make css py2/3 agnostic.

---
+++ tests/test_css.py
@@ -0,0 +1,20 @@
+from dk.html.css import css
+
+
+def test_css():
+    # the str here is bytes on py2 and unicode on py3
+
+    x = css(color='red', background='blue')
+    assert str(x) == 'background:blue;color:red'
+
+    x.border = 'none'
+    y = css(background='blue', color='red')
+    y.border = 'none'
+    assert x == y
+
+    a = css()
+    a['font-weight'] = 'bold'
+    assert str(a) == 'font-weight:bold'
+
+    a.text_align = 'right'
+    assert str(a) == 'font-weight:bold;text-align:right'
